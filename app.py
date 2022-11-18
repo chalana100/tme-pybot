d210dfdd0a7fa8d5cb32050af3a11ec44bca578c
chalana100 <chalanaweerasooriya100@gmail.com>
2022-11-18 18:47:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'fuck you {update.effective_user.last_name}')
 
 
-app = ApplicationBuilder().token("5307911209:AAGUKKcnZnTp6NDaMNLwXxRM1Wwx0Pb9_wc").build()
+app = ApplicationBuilder().token("5307911209:AAGa-PaMThD3Djzerw30XS1Pbm8n1TD8UbE").build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("help", help))
